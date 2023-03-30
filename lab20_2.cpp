94468559addbab1e655e6b514e9b28a5a487e177
TNC9 <koonbenzkub@hotmail.com>
2023-03-31 03:09:23 +0700

    Complete

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect* R1, Rect* R2){
+    double overlapY = min(R1->x + R1->w, R2->x + R2->w) - max(R1->x, R2->x);
+    double overlapX = min(R1->y, R2->y) - max(R1->y - R1->h, R2->y - R2->h);
+    double overlapArea = overlapX * overlapY;
+    return (overlapArea < 0) ? 0 : overlapArea;
 }
